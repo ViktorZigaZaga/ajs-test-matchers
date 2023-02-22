a8f48bb6f8e5049d60364c032026e3601e03802c
Viktor Bauer <bassa555@mail.ru>
2023-02-22 16:08:08 +0300

    fix error

--- src/__tests__/character.test.js
+++ src/__tests__/character.test.js
@@ -1,15 +1,15 @@
-import sortCharacter from '../character.js'
+import sortCharacter from '../character';
 
 test('should sort characters array', () => {
-    const result = sortCharacter([
-        {name: 'мечник', health: 10},
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-    ]);
+  const result = sortCharacter([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
 
-    expect(result).toEqual([
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-        {name: 'мечник', health: 10},
-      ]);
+  expect(result).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
 });
