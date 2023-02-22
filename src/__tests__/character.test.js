73ce3ee6b59ad4609535f29d8f240c930531c5da
Viktor Bauer <bassa555@mail.ru>
2023-02-22 15:54:33 +0300

    homefork test-matchers for review

---
+++ src/__tests__/character.test.js
@@ -0,0 +1,15 @@
+import sortCharacter from '../character.js'
+
+test('should sort characters array', () => {
+    const result = sortCharacter([
+        {name: 'мечник', health: 10},
+        {name: 'маг', health: 100},
+        {name: 'лучник', health: 80},
+    ]);
+
+    expect(result).toEqual([
+        {name: 'маг', health: 100},
+        {name: 'лучник', health: 80},
+        {name: 'мечник', health: 10},
+      ]);
+});
